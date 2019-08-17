5a2c98919062a56706502ac16fdd6ea57d86f786
Madelyn Deshield <madelynwallop@gmail.com>
2019-08-17 22:23:08 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -76,7 +76,7 @@ def all_supplies_in_holidays(holiday_hash)
      holidays.each do |holiday,supplies|
        puts "   #{holiday.to_s.split("_").map{|word|word.capitalize}.join(" ")}: #{supplies.join(",") }"
        
-      binding.pry
+      
       
       
  end   
