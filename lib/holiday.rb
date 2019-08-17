47de1694628537da29b5acd746e32b31ffc1b10e
Madelyn Deshield <madelynwallop@gmail.com>
2019-08-17 22:54:05 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -81,6 +81,7 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+  binding.pry
     holiday_hash[:summer][:momerial_day]
     puts holiday_hash
 end
